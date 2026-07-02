5cb2459f7de62df40e6447242b55d863365fa567
Agnes Aurora Marinho da Silva Cavalcanti <vas0037579@aluno.firjansenaisesi.com.br>
2026-07-01 21:27:30 -0300

    1.0

--- src/Components/ComponentPicker.jsx
+++ src/Components/ComponentPicker.jsx
@@ -8,7 +8,10 @@ export default function ComponentPicker(){
         portuguesa: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/2eb7ece4ae9a67b773aa138589e2031d.webp?itok=8rB5qKP-',
         bacon: 'https://www.seara.com.br/wp-content/uploads/2025/09/R0325-SL-pizza-de-bacon-fatiado-cebola-caramelizada-1200x675-1.webp',
         frango_catupiry: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/85edcfdb1dae604c2627a88c06fa0c85.webp?itok=GL3lIKtV',
-        alho: ''
+        alho: 'https://mrteddypizza.com.br/wp-content/uploads/2024/03/Design-sem-nome-2024-03-21T014935.418.png',
+        mussarela: 'https://www.receitasnestle.com.br/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/fe1087b2f7b47b6a5bc8ad8269601a97.webp?itok=olj7rz2i',
+        camarao: 'https://www.sabornamesa.com.br/media/k2/items/cache/08a4cf1e44f783c853b207a033a173b9_XL.webp',
+        marguerita: ''
     }
 
     const [sabor, setSabor] = useState('')
